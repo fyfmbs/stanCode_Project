e5d8af40916c5831323ed3eee944e63743cb7858
Shawn Chan <cc698@njit.edu>
2021-04-13 14:04:46 +0800

    Update anagram.py

--- boggle_game_solver/anagram.py
+++ boggle_game_solver/anagram.py
@@ -15,35 +15,29 @@ number of anagrams for each word listed below:
     * tesla -> 10 anagrams
     * spear -> 12 anagrams
 """
+import time
 
 # Constants
 FILE = 'dictionary.txt'       # This is the filename of an English dictionary
 EXIT = '-1'                   # Controls when to stop the loop
 
 # Global Variable
-sum_count = 0
+
+prefix_dict = {}
 
 
 def main():
-
+    print('Welcome to stanCode "Anagram Generator" (or -1 to quit)')
     while True:
-        new_word_lst = []
-        count = 0
-        print('Welcome to stanCode "Anagram Generator" (or -1 to quit)')
         word = input('Find anagram for: ')
-        word = word.lower()
-        word_dic = read_dictionary(len(word))
         if word == EXIT:
             break
+        start_time = time.time()
+        word = word.lower()
         print('Searching....')
-        word_lst = find_anagrams(word)
-        for new_word in word_lst:
-            if new_word in word_dic:
-                new_word_lst.append(new_word)
-                count += 1
-                print('Found', new_word)
-                print('Searching....')
-        print(count, 'anagrams: ', new_word_lst)
+        word_lst = find_anagrams(sorted(word))
+        print(len(word_lst), 'anagrams: ', word_lst)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
 
 def read_dictionary(num):
@@ -51,13 +45,20 @@ def read_dictionary(num):
     Create a list for saving the whole dictionary
     :return: list of dictionary
     """
-    word_dic = []
+    global prefix_dict
+    prefix_dict = {}
     with open('dictionary.txt', 'r') as f:
-        for line in f:
-            word = line.strip()
+        # remove the line breaker '\n'
+        words = f.read().splitlines()
+        for word in words:
             if len(word) == num:
-                word_dic.append(word)
-    return word_dic
+                for i in range(len(word)-1):
+                    # store all prefix of word (including it self) in prefix_dict
+                    prefix = word[:i+1]
+                    if prefix not in prefix_dict:
+                        prefix_dict[prefix] = prefix
+                prefix_dict[word] = word
+    return prefix_dict
 
 
 def find_anagrams(s):
@@ -67,37 +68,36 @@ def find_anagrams(s):
     """
     alphabet_dic = create_alphabet_dic(s, {})
     # list for counting how many times for using find_anagrams_helper
-    cnt_lst = []
-    word_dic = read_dictionary(len(s))
-    word_lst = find_anagrams_helper(alphabet_dic, '', [], [], cnt_lst, word_dic)
+    read_dictionary(len(s))
+    word_lst = find_anagrams_helper(alphabet_dic, '', [], [])
     return word_lst
 
 
-def find_anagrams_helper(alphabet_dic, c_s, c_n, word_lst, cnt_lst, word_dic):
+def find_anagrams_helper(alphabet_dic, c_s, c_n, word_lst):
     """
     a helper for the find_anagrams that can input more info
-    :param word_dic:
     :param alphabet_dic: the dictionary include every alphabet in the searching word
     :param c_s: a string for saving the word that we search
     :param c_n: a list for saving the index of the dictionary
     :param word_lst: a list for saving the word that I searched
-    :param cnt_lst: the list that saving the using times
     :return: the word list that we searched
     """
-    cnt_lst.append(1)
+
     if len(c_s) == len(alphabet_dic):
         if c_s not in word_lst:
             word_lst.append(c_s)
+            print('Found', c_s)
+            print('Searching....')
 
     else:
         for ele in alphabet_dic:
             if ele not in c_n:
                 c_n.append(ele)
                 c_s += alphabet_dic[ele]
-                if has_prefix(c_s, word_dic) is True:
-                    find_anagrams_helper(alphabet_dic, c_s, c_n, word_lst, cnt_lst, word_dic)
+                if has_prefix(c_s) is True:
+                    find_anagrams_helper(alphabet_dic, c_s, c_n, word_lst)
                     c_n.pop()
-                    c_s = c_s[:len(c_s)-1]
+                    c_s = c_s[:len(c_s) - 1]
                 else:
                     c_n.pop()
                     c_s = c_s[:len(c_s) - 1]
@@ -117,16 +117,16 @@ def create_alphabet_dic(word, alphabet_dic):
     return alphabet_dic
 
 
-def has_prefix(sub_s, word_dic):
+def has_prefix(sub_s):
     """
-    :param word_dic:
     :param sub_s: the sub_string with the staring word
     :return: True of False
     """
-    for word in word_dic:
-        if word.startswith(sub_s) is True:
-            return True
-    return False
+    if sub_s in prefix_dict:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     main()
